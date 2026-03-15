50caaa0edad79479bf17a2925e5ceef079d9ed82
AbdulAmmic <connectorstech@gmail.com>
2026-03-15 21:21:44 +0100

    shhh

--- app.py
+++ app.py
@@ -78,6 +78,7 @@ def create_app():
 
     return app
 
+app = create_app()
+
 if __name__ == '__main__':
-    app = create_app()
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=os.getenv('PORT', 5000))
